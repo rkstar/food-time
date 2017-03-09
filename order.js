8148e3f93bca7aaf4877b10f3c9248320a63bc21
David Fudge <rkstar@mac.com>
2017-03-08 19:26:41 -0500

    added event listeners and refactored html to make it easier to get and add options later

--- order.js
+++ order.js
@@ -1,7 +1,27 @@
-(function($){$(function(){
-
-  // hide our js-food-options
+function hideMenuOptions(){
   $(".js-food-options").hide()
+}
+
+function buildOptionsId(category){
+  return "#"+category+"-options"
+}
+function handleFoodCategoryChange(e){
+  hideMenuOptions()
+  $(buildOptionsId(e.target.value)).show()
+}
+
+function listenForFoodItemChange(){
+  $("#food").on("change", handleFoodCategoryChange)
+}
+
+function initFoodMenu(){
+  // hide our js-food-options
+  hideMenuOptions()
+  
+  // add event listener to select list
+  listenForFoodItemChange()
+}
 
 
-})})(jQuery)
+
+(function(){$(initFoodMenu)})()
